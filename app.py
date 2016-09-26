07699d70d7550ee84bbc4cd48c2bc3a49eecf7d8
Danny <flackdl@gmail.com>
2016-09-26 02:00:23 +0000

    further

--- app.py
+++ app.py
@@ -2,13 +2,28 @@ import os
 from cStringIO import StringIO
 from flask import render_template, render_template_string, Response
 from weasyprint import HTML
-from flask import Flask
+from flask import Flask, request
+from quickbooks import QuickBooks
+import secret  # not in version control. should define token, key & secret
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+def login():
+    client = QuickBooks(
+        sandbox=True,
+        consumer_key=secret.production_key,
+        consumer_secret=secret.production_secret,
+        callback_url='http://%s/callback' % request.host,
+    )
+    authorize_url = client.get_authorize_url()
+    request_token = client.request_token
+    return render_template('login.html', authorize_url=authorize_url)
+    
+@app.route('/callback')
+def callback():
+    return request.args.get()
+    
     
 @app.route('/html/<int:po_id>')
 def html(po_id):
@@ -31,4 +46,5 @@ def pdf(po_id):
     return Response(pdf.getvalue(), mimetype='application/pdf')
 
 app.config['DEBUG'] = True
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+if __name__ == "__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
