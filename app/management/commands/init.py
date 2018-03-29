afc18c8b2263b238d2d46f2a2b9306fbeb43ca9d
danny flack <flackattack@gmail.com>
2018-03-28 20:51:08 -0400

    update "init" script to set "pos" permissions

--- app/management/commands/init.py
+++ app/management/commands/init.py
@@ -1,11 +1,26 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.core.management import BaseCommand
 from django.conf import settings
+from app.models import Order, OrderEstimatePart
 
 
 class Command(BaseCommand):
     help = 'Init'
 
     def handle(self, *args, **options):
-        # create pos user
-        User.objects.create_user(settings.POS_USER, password=settings.POS_PASSWORD)
+        # create user and set permissions
+        users = User.objects.filter(username=settings.POS_USER)
+        if users.exists():
+            user = users[0]
+        else:
+            # create pos user
+            user = User.objects.create_user(settings.POS_USER, password=settings.POS_PASSWORD)
+
+        actions = ['add', 'change', 'delete']
+        models = [Order, OrderEstimatePart]
+        perms = []
+        for model in models:
+            for action in actions:
+                perms += Permission.objects.filter(codename__in=[
+                        '{}_{}'.format(action, model._meta.model_name)])
+        user.user_permissions.set([p for p in perms])  # iterate because it doesn't expect a Queryset
