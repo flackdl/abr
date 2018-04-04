8966113d604523b65be9d4a07dfd93572ea025ce
danny flack <flackattack@gmail.com>
2018-04-04 13:03:15 -0400

    admin

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from app.models import Order, OrderPart
 
 
-@admin.register(Order, OrderPart)
-class Admin(admin.ModelAdmin):
-    pass
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('date_created', 'arrived', 'order_id', 'vendor',)
+    list_filter = ('date_created', 'arrived', 'order_id', 'vendor',)
+
+
+@admin.register(OrderPart)
+class OrderPartAdmin(admin.ModelAdmin):
+    # note the distinction of "order arrived":
+    # - the filter is a reverse query
+    # - the display is a function lookup because it doesn't support the reverse lookup
+    list_display = ('order', 'order_arrived', 'estimate_id', 'part_id',)
+    list_filter = ('order', 'order__arrived', 'estimate_id', 'part_id',)
+
+    def order_arrived(self, part):
+        """
+        :type part: OrderPart
+        """
+        return part.order.arrived
