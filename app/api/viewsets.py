e7dc4fb7b1f42c680efafc8ffc64ccef09ead336
danny flack <flackattack@gmail.com>
2018-04-04 20:23:37 -0400

    update existing vendor/order_id vs throwing unique db error

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from app.models import Order, OrderPart
 from app.api.serializers import OrderSerializer, OrderPartSerializer
 
@@ -8,13 +9,34 @@ class OrderViewset(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     filter_fields = ('arrived',)
 
+    def create(self, request, *args, **kwargs):
+        # if adding parts and a vendor/order_id combination already exists, assign the the parts to it instead
+        existing_vendor_order = Order.objects.filter(vendor=request.data.get('vendor'), order_id=request.data.get('order_id'))
+        if 'order_parts' in request.data and existing_vendor_order.exists():
+            serializer = OrderSerializer(data=request.data, instance=existing_vendor_order[0])
+            serializer.is_valid(raise_exception=True)
+            serializer.instance.description = '{}\n\nDescription updated when parts were added to order:\n\n{}'.format(
+                serializer.instance.description,
+                request.data.get('description'),
+            )
+            serializer.instance.save()
+            self.assign_parts(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return super(OrderViewset, self).create(request, *args, **kwargs)
+
     def perform_create(self, serializer):
         """
-        :rtype serializer: ModelSerializer
+        :type serializer: ModelSerializer
         """
         # save and then attach parts to the order
         super(OrderViewset, self).perform_create(serializer)
-        order_parts_data = serializer.initial_data.get('order_parts')
+        if 'order_parts' in serializer.initial_data:
+            self.assign_parts(serializer)
+
+    @staticmethod
+    def assign_parts(serializer):
+        order_parts_data = serializer.initial_data['order_parts']
         # attach the order instance to each estimate/part record
         for estimate_part in order_parts_data:
             estimate_part['order'] = serializer.instance.id
