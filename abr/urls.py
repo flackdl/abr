cd18e311c05140a89100989209e6c0c214775147
dormond <mainheart@com.com>
2020-01-20 15:30:58 -0600

    rm unused import

--- abr/urls.py
+++ abr/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
 
 from app import views
 import app.api.urls
