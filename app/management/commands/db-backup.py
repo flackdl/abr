34fcd8fc54cd790cb0386e8e162c7e8f54f425d2
dormond <mainheart@com.com>
2020-03-20 13:20:44 -0500

    new backup folder

--- app/management/commands/db-backup.py
+++ app/management/commands/db-backup.py
@@ -14,4 +14,4 @@ class Command(BaseCommand):
         s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
         out = StringIO()
         management.call_command(dumpdata.Command(), 'app', stdout=out)
-        s3_client.put_object(Body=out.getvalue(), Bucket='abr-backups', Key='db-{}.json'.format(datetime.now().day))
+        s3_client.put_object(Body=out.getvalue(), Bucket='abr-backups', Key='new/db-{}.json'.format(datetime.now().day))
